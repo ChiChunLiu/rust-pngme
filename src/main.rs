d33ae4c9564bb1fad50c86151d1e8f07ca62aaee
ccl <cliu@ccliu.me>
2023-03-16 00:49:18 +0800

    Add cli scaffold with stub implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,60 @@
 mod chunk;
 mod chunk_type;
 mod png;
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
-fn main() -> Result<()> {
-    todo!()
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Encode {
+        config: PathBuf,
+        chunk_type: String,
+        message: String,
+        output_file: Option<PathBuf>,
+    },
+    Decode {
+        config: PathBuf,
+        chunk_type: String,
+    },
+    Remove {
+        config: PathBuf,
+        chunk_type: String,
+    },
+    Print {
+        config: PathBuf,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Encode {
+            config,
+            chunk_type,
+            message,
+            output_file,
+        } => {
+            println!("encoded: {chunk_type:?} {message:?}")
+        }
+        Commands::Decode { config, chunk_type } => {
+            println!("decoded: {chunk_type:?}")
+        }
+        Commands::Remove { config, chunk_type } => {
+            println!("removed: {chunk_type:?} ")
+        }
+        Commands::Print { config } => {
+            println!("content")
+        }
+    }
 }
